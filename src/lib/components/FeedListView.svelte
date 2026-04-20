48eb5120ad087d6c677c53913e9953f21667a4f4
Jascha030 <contact@jaschavanaalst.nl>
2026-04-20 21:36:53 +0200

    🎨 Lil cleanup

--- src/lib/components/FeedListView.svelte
+++ src/lib/components/FeedListView.svelte
@@ -480,6 +480,7 @@
 						style={`height: ${rowHeight}px; transform: translateY(${top}px);`}
 					>
 						{#if item}
+							<!-- svelte-ignore a11y_no_noninteractive_element_interactions, a11y_click_events_have_key_events -->
 							<article
 								class={`feed-row relative flex h-full min-h-0 flex-col overflow-hidden px-6 py-5 transition-colors duration-150 lg:px-8 ${
 									index > 0 ? 'border-t border-border' : ''
@@ -534,6 +535,7 @@
 										{/if}
 									</div>
 
+									<!-- svelte-ignore a11y_no_static_element_interactions -->
 									<div class="flex flex-wrap gap-2" onclick={(e) => e.stopPropagation()}>
 										{#if isMediaItem(item)}
 											<DynamicPlayButton {item} compact={true} size="sm" />
@@ -548,32 +550,9 @@
 												}}
 											>
 												{#if item.read}
-													<svg
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 16 16"
-														fill="currentColor"
-														class="size-4"
-													>
-														<path
-															fill-rule="evenodd"
-															d="M1.756 4.568A1.5 1.5 0 0 0 1 5.871V12.5A1.5 1.5 0 0 0 2.5 14h11a1.5 1.5 0 0 0 1.5-1.5V5.87a1.5 1.5 0 0 0-.756-1.302l-5.5-3.143a1.5 1.5 0 0 0-1.488 0l-5.5 3.143Zm1.82 2.963a.75.75 0 0 0-.653 1.35l4.1 1.98a2.25 2.25 0 0 0 1.955 0l4.1-1.98a.75.75 0 1 0-.653-1.35L8.326 9.51a.75.75 0 0 1-.652 0L3.575 7.53Z"
-															clip-rule="evenodd"
-														/>
-													</svg>
+													<Icon icon="heroicons:envelope-open-solid" class="size-5" />
 												{:else}
-													<svg
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 16 16"
-														fill="currentColor"
-														class="size-4"
-													>
-														<path
-															d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
-														/>
-														<path
-															d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
-														/>
-													</svg>
+													<Icon icon="heroicons:envelope-solid" class="size-5" />
 												{/if}
 											</button>
 										{/if}
